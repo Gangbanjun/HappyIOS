1ef640eae45af03d03d13806d68e38c62b6e64b1
Gangbanjun <1258107412@qq.com>
2018-12-03 22:41:13 +0800

    将工程提交仓库

---
+++ HappyIOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HappyIOS
+//
+//  Created by Air on 2018/12/3.
+//  Copyright © 2018年 YC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HappyIOS/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  HappyIOS
+//
+//  Created by Air on 2018/12/3.
+//  Copyright © 2018年 YC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+
+@end
